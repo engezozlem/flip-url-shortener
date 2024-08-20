efb56705147a2fa041789582d0f63425b277b527
ozlem.engez <ozlem.engez@trendyol.com>
2024-08-20 19:42:40 +0300

    fix asterisk imports

--- src/main/java/com/flip/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/com/flip/urlshortener/controller/UrlShortenerController.java
@@ -15,7 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.HashMap;
 import java.util.Map;
--- src/test/java/com/flip/urlshortener/service/UrlShortenerServiceImplTest.java
+++ src/test/java/com/flip/urlshortener/service/UrlShortenerServiceImplTest.java
@@ -4,7 +4,10 @@ import com.flip.urlshortener.exception.UrlNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class UrlShortenerServiceImplTest {
 
